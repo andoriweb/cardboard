d7daeac18774b6234add524bdc05e3052139f0e3
andoryweb <andoryweb@gmail.com>
2020-05-25 12:12:53 +0300

    section production

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -14,3 +14,61 @@ $(document).ready(function(){
     ]
   });
 });
+
+$('.production-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.production-slider-small',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<button class="production-arrow production-prev production-arrow__mob"></button>',
+        nextArrow: '<button class="production-arrow production-next production-arrow__mob"></button>'
+      }
+    }
+  ]
+});
+$('.production-slider-small').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.production-slider',
+  dots: false,
+  arrows: true,
+  prevArrow: '<button type="button" class="production-arrow production-prev"></button>',
+  nextArrow: '<button type="button" class="production-arrow production-next"></button>',
+  centerMode: true,
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 890,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 801,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: 'unslick'
+    }
+  ]
+});
